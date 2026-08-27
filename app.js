72d2b9ef1f58f5c817bae0736262797435538dab
miguelangel8888 <mkalyu81@gmail.com>
2026-08-27 00:02:26 -0400

    Se mejoro la interfaz y se agrego la funcion de mostrar la palabra

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const musica = document.querySelector('.musica')
 
-musica.volume = 0.1
-musica.loop = true
+musica.volume = 0.001
+musica.loop = false
 
 document.addEventListener('click', () =>{
     musica.play()
@@ -11,3 +11,20 @@ window.addEventListener('beforeunload', () => {
     musica.pause()
     musica.currentTime=0
 })
+
+let arregloEspacios = [];
+
+const palabra = document.getElementById('palabra')
+const boton = document.querySelector(".btnIngresarPalabra")
+const adivPalabra = document.querySelector(".palabraAdivinar")
+
+boton.addEventListener('click',()=>{
+    arregloEspacios=[]
+    let letras = palabra.value
+    let letrasPalabra = letras.split("")
+    letrasPalabra.map((letra)=>{
+        arregloEspacios.push("_")
+    })
+    adivPalabra.innerHTML = `<p><strong>${arregloEspacios.join(" ")}</strong></p>` 
+});
+
