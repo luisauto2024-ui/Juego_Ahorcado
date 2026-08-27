943e90236315d7f3a4634fa50f3263cb53e17827
miguelangel8888 <mkalyu81@gmail.com>
2026-08-27 00:40:37 -0400

    agregando funcion de llenado de letras

--- app.js
+++ app.js
@@ -12,19 +12,38 @@ window.addEventListener('beforeunload', () => {
     musica.currentTime=0
 })
 
+let letrasPalabra 
 let arregloEspacios = [];
 
 const palabra = document.getElementById('palabra')
 const boton = document.querySelector(".btnIngresarPalabra")
 const adivPalabra = document.querySelector(".palabraAdivinar")
+const inputletraAdiv = document.getElementById('letra')
+const botonSelecLetra = document.querySelector(".btnIngresarletra")
 
 boton.addEventListener('click',()=>{
     arregloEspacios=[]
     let letras = palabra.value
-    let letrasPalabra = letras.split("")
+    letrasPalabra = letras.split("")
     letrasPalabra.map((letra)=>{
         arregloEspacios.push("_")
     })
     adivPalabra.innerHTML = `<p><strong>${arregloEspacios.join(" ")}</strong></p>` 
+
 });
+
+botonSelecLetra.addEventListener('click',()=>{
+    let letra = inputletraAdiv.value
+    let cont = 0;
+    for (const valor of letrasPalabra) {
+        
+        if(valor === letra){
+            arregloEspacios[cont]=letra
+        }else{
+            cont++
+        }
+    }
+    adivPalabra.innerHTML = `<p><strong>${arregloEspacios.join(" ")}</strong></p>` 
+    
+})
 
